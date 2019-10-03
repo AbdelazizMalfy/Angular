45b30bb3dfbc62df1b689d9c1882de81715272c3
Abdelaziz <abdelazizMalfy@gmail.com>
2019-10-03 13:13:37 +0200

    orders component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    OrdersComponent,
   ],
   imports: [
     BrowserModule,
