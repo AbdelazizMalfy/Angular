ccfb3542c95e8b4f80a6bc9cf6e7dd94d46c697e
Abdelaziz <abdelazizMalfy@gmail.com>
2019-10-03 11:17:52 +0200

    using data service to get customers in the customers componen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CustomersModule } from './Customers/customers.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     CustomersModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -4,15 +4,35 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map , catchError } from 'rxjs/operators';
 
+import { ICustomer } from '../shared/interfaces';
+
 
 @Injectable()
 export class DataService {
-    baseUrl: string = 'assets/'
+    baseUrl: string = 'assets'
 
-    constructor(){
+    constructor(private http: HttpClient){}
 
+
+    // THIS REQUEST GETS ALL CUSTOMERS
+    getCustomers() : Observable<ICustomer[]> {
+        return this.http.get<ICustomer[]>(this.baseUrl + '/customers.json')
+            .pipe(
+                catchError(this.handleError)
+            )
     }
 
+    // THIS REQUEST GETS ONLY ONE CUSTOMER
+    getCustomer(id: number): Observable<ICustomer>  {
+        return this.http.get<ICustomer[]>(this.baseUrl + '/orders.json')
+            .pipe(
+                map(customers => {
+                    let customer = customers.filter((cust:ICustomer)=> cust.id === id)
+                    return (customer && customer.length) ? customer[0] : null;
+                }),
+                catchError(this.handleError)
+            )
+    }
 
     private handleError(error: any) {
         console.error('server error:', error);
