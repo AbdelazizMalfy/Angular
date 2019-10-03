dd62a9744891cad21f56d1a9af09fb4dd98f2f23
Abdelaziz <abdelazizMalfy@gmail.com>
2019-10-03 12:15:53 +0200

    customers route added

--- src/app/Customers/customers.module.ts
+++ src/app/Customers/customers.module.ts
@@ -7,9 +7,10 @@ import { SharedModule } from '../shared/shared.module'
 import {CustomersComponent} from './customerscomponent';
 import { CustomersListComponent } from './customers-list/customers-listcomponent';
 import { FilterTextboxComponent } from './customers-list/filter-textboxcomponent';
+import { CustomerRoutingModule } from './customer-routing.module'
 
 @NgModule({
-    imports: [ CommonModule, SharedModule, FormsModule ],
+    imports: [ CommonModule, SharedModule, FormsModule , CustomerRoutingModule ],
     declarations: [ CustomersComponent , CustomersListComponent , FilterTextboxComponent],
     exports: [ CustomersComponent    ]
 })
